5c3065cbe01c875894e913a23bdd3a4ac4d857be
Thiago Galvani <thiagopaiva99@gmail.com>
2019-11-27 20:59:07 -0200

    Creating the navbar for the application

--- hello-world-microfront/single-spa.config.js
+++ hello-world-microfront/single-spa.config.js
@@ -7,12 +7,15 @@ function pathPrefix(prefix) {
 }
 
 registerApplication(
-  // Name of our single-spa application
   'home',
-  // loadingFunction
   () => import('./src/home/home.app.js'),
-  // activityFunction
-  pathPrefix('/home')
+  pathPrefix('/')
+);
+
+registerApplication(
+  'navBar', 
+  () => import('./src/navBar/navBar.app.js').then(module => module.navBar),
+  () => true
 );
 
 start();
